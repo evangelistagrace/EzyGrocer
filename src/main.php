d1adb1b82dbd156b06f492e422f271b8235d71ff
evangelistagrace <42605267+evangelistagrace@users.noreply.github.com>
2020-01-21 05:37:56 +0800

    categories display

---
+++ src/main.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>EzyGrocer</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="../assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="../assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+
+<body>
+    <!-- header -->
+    <nav class="navbar navbar-light text-primary bg-light navigation-clean-search">
+        <div class="container-fluid"><i class="fa fa-bars"></i><img class="navbar-logo" src="../assets/img/4.png"><i
+                class="fa fa-search"></i></div>
+    </nav>
+
+    <div class="container">
+        <div class="row display-column">
+            <div class="col">
+                <h5>Search Categories</h5>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="col cat cat-1">
+                        <desc>Poultry</desc>
+                    </div>
+                    <div class="col cat cat-2">
+                        <desc>Vegetables</desc>
+                    </div>
+                    <div class="col cat cat-3">
+                        <desc>Baking</desc>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <h5>Search Recipes</h5>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="col-3 offset-0"></div>
+                    <div class="col-3 offset-0"></div>
+                    <div class="col-3 offset-0"></div>
+                    <div class="col-3 offset-0"></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- footer -->
+    <nav class="navbar navbar-light navbar-expand fixed-bottom text-primary bg-light navigation-clean navbar-footer">
+        <div class="container container-footer"><button class="navbar-toggler" data-toggle="collapse"><span
+                    class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <ul class="menu-footer">
+                <li><i class="icon ion-android-home"></i></li>
+                <li><i class="icon ion-chatbubble-working"></i></li>
+                <li><i class="fa fa-shopping-cart"></i></li>
+                <li><i class="fa fa-gear"></i></li>
+            </ul>
+        </div>
+    </nav>
+
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
